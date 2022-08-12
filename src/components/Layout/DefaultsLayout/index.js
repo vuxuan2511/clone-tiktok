cc51bd7224365c9d158bd492ef732756556c4138
vuxuan2511 <vuxuan183@gmail.com>
2022-08-12 15:25:13 +0700

    dung khung layout

--- src/components/Layout/DefaultsLayout/index.js
+++ src/components/Layout/DefaultsLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind';
+import style from './DefaultLayout.module.scss';
 import Header from '../conponent/Header';
 import Sidebar from './Sidebar';
 
+const cx = classNames.bind(style);
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
